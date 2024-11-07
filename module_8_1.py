cf51f6664134d1608253eb734fa78ca1e612b9d9
sanych2891 <sanych2891@icloud.com>
2024-11-07 13:23:55 +1000

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,15 @@
+
+# Задание "Программистам всё можно":
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
+
+# Примеры использования
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
+
